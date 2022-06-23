efe3eae6e6a7d914452bc7a7f07d350728e4b705
Ttibsi <ashisbitt@icloud.com>
2022-06-23 11:23:40 +0100

    Hello world

---
+++ basics/hello_world.cpp
@@ -0,0 +1,9 @@
+#include "iostream" // This is an import statement 
+
+using namespace std; //Bringing the standard library into scope
+
+int main() // Create a function that returns an int
+// Main will always return the exit code
+{
+    cout << "hello world\n";
+}
